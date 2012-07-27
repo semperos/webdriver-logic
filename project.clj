50aa552b7ccc30e36f60f6318bd86926c8f68f48
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-07-27 00:48:29 -0400

    Upgrade core.logic from original commit

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject webdriver-logic "0.0.1-SNAPSHOT"
   :description "Library for composing clj-webdriver tests using logic programming"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.logic "0.6.5"]
+                 [org.clojure/core.logic "0.7.6-SNAPSHOT"]
                  [clj-webdriver "0.6.0-SNAPSHOT"]
                  [enlive "1.0.1"]])
