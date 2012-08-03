b3586c2ded8162260d9721b796c39fb15cfc1bf7
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-08-03 19:52:23 -0400

    Benchmarks will act as examples

--- test/webdriver_logic/test/examples/core.clj
+++
@@ -1,8 +0,0 @@
-(ns webdriver-logic.examples.core
-  (:refer-clojure :exclude [==])
-  (:use webdriver-logic.core :reload
-        clojure.core.logic)
-  (:require [clj-webdriver.core :as wd]))
-
-(comment
-  (def b (wd/start {:browser :chrome} "https://github.com")))
