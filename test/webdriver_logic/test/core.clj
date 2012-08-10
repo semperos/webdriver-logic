06e9f95d3943297dd54bc85381550e9991016b7c
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-08-10 10:49:41 -0400

    Continue test coverage of relations

--- test/webdriver_logic/test/core.clj
+++ test/webdriver_logic/test/core.clj
@@ -41,25 +41,25 @@
 ;;
 ;; Yes, these do beg the question.
 ;;
-;; Also, `*search-domain*` get rebound often due to performance reasons.
+;; Also, `*search-domain*` gets rebound often due to performance reasons.
 ;;
 
 (deftest test-basic-logic-success
   (s (run* [q]
            (== q true))))
 
-;; This test doubles as a test of all the s/u test macros
+;; This test doubles as a test of all the s/u test macros.
 (deftest test-attributeo
-  ;; There is an element with id "pages-table"
+  ;; There is an element with id "pages-table".
   (s (run* [q]
            (attributeo q :id "pages-table")))
-  ;; There is no element with id "no-such-id"
+  ;; There is no element with id "no-such-id".
   (u (run* [q]
            (attributeo q :id "no-such-id")))
-  ;; There are multiple elements with class "external"
+  ;; There are multiple elements with class "external".
   (s+ (run* [q]
             (attributeo q :class "external")))
-  ;; All goal values returned from the run equal "class"
+  ;; All goal values returned from the run equal "class".
   (s? (fn [goal-vals]
         (every? #(= % "class") goal-vals))
       (binding [*search-domain* {:css "#content > p *"}]
@@ -67,28 +67,28 @@
              (fresh [el attr]
                     (attributeo el attr "external")
                     (== q attr)))))
-  ;; Of all attributes with value "external", "class" is one
+  ;; Of all attributes with value "external", "class" is one.
   (s-includes ["class"]
               (binding [*search-domain* {:css "#content > p *"}]
                 (run 2 [q]
                      (fresh [el attr]
                             (attributeo el attr "external")
                             (== q attr)))))
-  ;; Only the "class" attribute has a value of "external" for all elements
+  ;; Only the "class" attribute has a value of "external" for all elements.
   (s-as ["class"]
               (binding [*search-domain* {:css "#content > p *"}]
                 (distinct (run 2 [q]
                                (fresh [el attr]
                                       (attributeo el attr "external")
                                       (== q attr))))))
-  ;; Alternative syntax for single-value checks
+  ;; Alternative syntax for single-value checks.
   (s-as "class"
               (binding [*search-domain* {:css "#content > p *"}]
                 (distinct (run 2 [q]
                                (fresh [el attr]
                                       (attributeo el attr "external")
                                       (== q attr))))))
-  ;; Id's on the page include "pages" and "pages-table"
+  ;; Id's on the page include "pages" and "pages-table".
   (s-includes ["pages" "pages-table"]
               (binding [*search-domain* {:css "#content > *"}]
                 (run* [q]
@@ -97,7 +97,78 @@
                              (== q value))))))
 
 (deftest test-childo
+  ;; The content div has multiple children.
   (s+ (run 3 [q]
            (childo q (wd/find-element driver {:css "#content"}))))
+  ;; A direct child anchor tag of a direct child paragraph tag of the element
+  ;; with id `content` has multiple parents.
   (s+ (run 2 [q]
            (childo (wd/find-element driver {:css "#content > p > a"}) q))))
+
+(deftest test-displayedo
+  ;; The page contains multiple elements that are displayed (visible).
+  (s+ (run 2 [q]
+           (displayedo q)))
+  ;; The first anchor tag with a class of external is displayed (visible).
+  (s (run* [q]
+           (displayedo (wd/find-element driver {:css "a.external"})))))
+
+(deftest test-enabledo
+  ;; Go to the page with forms on it.
+  (wd/click (wd/find-element driver {:text "example form"}))
+  ;; The page contains multiple elements that are enabled.
+  (s+ (run 2 [q]
+           (enabledo q)))
+  ;; The first input element is enabled.
+  (s (run* [q]
+           (enabledo (wd/find-element driver {:css "input"}))))
+  ;; The input element with id `disabled_field` is not enabled.
+  (u (run* [q]
+           (enabledo (wd/find-element driver {:css "input#disabled_field"})))))
+
+(deftest test-existso
+  ;; At least two elements exist.
+  (s+ (run 2 [q]
+           (existso q)))
+  ;; An element with class `external` does exist.
+  (s (run* [q]
+           (existso (wd/find-element driver {:class "external"}))))
+  ;; An element with id `no-such-id` does not exist.
+  (u (run 1 [q]
+          (existso (wd/find-element driver {:id "no-such-id"})))))
+
+(deftest test-presento
+  ;; At least two elements are present.
+  (s+ (run 2 [q]
+           (presento q)))
+  ;; The first element with class `external` is present.
+  (s (run* [q]
+           (presento (wd/find-element driver {:class "external"}))))
+  ;; The first anchor tag with an `href` of `#pages` exists...
+  (s (run* [q]
+           (existso (wd/find-element driver {:tag :a, :href "#pages"}))))
+  ;; ...but is not visible...
+  (u (run* [q]
+           (displayedo (wd/find-element driver {:tag :a, :href "#pages"}))))
+  ;; ...and thus is not present.
+  (u (run* [q]
+           (presento (wd/find-element driver {:tag :a, :href "#pages"}))))
+  ;; And together:
+  (is (= (run* [q]
+               (existso (wd/find-element driver {:tag :a, :href "#pages"}))
+               (displayedo (wd/find-element driver {:tag :a, :href "#pages"})))
+         (run* [q]
+               (presento (wd/find-element driver {:tag :a, :href "#pages"})))
+         ())))
+
+;; TODO:
+;;
+;;  * selectedo
+;;  * sizeo
+;;  * tago
+;;  * texto
+;;  * valueo
+;;  * visibleo
+;;
+;; And then tests combining all relations in various orders with
+;; ground and fresh variables in all possible positions.
