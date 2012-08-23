17438c80652647fd922ffc4bd5347fdda953af24
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-08-22 21:10:08 -0400

    Use latest of core.logic and clj-webdriver

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject webdriver-logic "0.0.1-SNAPSHOT"
   :description "Library for composing clj-webdriver tests using logic programming"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.logic "0.8-alpha1"]
-                 [clj-webdriver "0.6.0-SNAPSHOT"]
+                 [org.clojure/core.logic "0.8-alpha2"]
+                 [clj-webdriver "0.6.0-alpha11"]
                  [enlive "1.0.1"]]
   :dev-dependencies [[criterium "0.2.1"]
                      [ring "1.1.1"]
