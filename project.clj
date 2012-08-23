e7fb0d4bf910082c0dfe345d9294dbb6c4832b56
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-08-22 21:12:43 -0400

    Upgrade to Leiningen 2

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
 (defproject webdriver-logic "0.0.1-SNAPSHOT"
   :description "Library for composing clj-webdriver tests using logic programming"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.logic "0.8-alpha2"]
                  [clj-webdriver "0.6.0-alpha11"]
                  [enlive "1.0.1"]]
-  :dev-dependencies [[criterium "0.2.1"]
-                     [ring "1.1.1"]
-                     [enlive "1.0.1"]
-                     [net.cgrand/moustache "1.1.0"]])
+  :profiles {:dev
+             {:dependencies
+              [[criterium "0.2.1"]
+               [ring "1.1.1"]
+               [enlive "1.0.1"]
+               [net.cgrand/moustache "1.1.0"]]}})
