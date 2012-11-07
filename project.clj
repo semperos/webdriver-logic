70bac1470fd9bb9350b5cc46ec2e3fe77a124f98
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-11-06 20:57:03 -0500

    Upgrade clj-webdriver to 0.6.0-beta2

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.logic "0.8.0-beta1"]
-                 [clj-webdriver "0.6.0-beta1"]
+                 [clj-webdriver "0.6.0-beta2"]
                  ;; [enlive "1.0.1"]
                  [org.clojars.semperos/enlive "1.0.1"]
                  ]
