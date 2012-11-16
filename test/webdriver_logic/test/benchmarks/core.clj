3b1c5ac04dbc78ca512990d48be9c344d225d1ce
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-11-15 23:55:08 -0500

    Update benchmarks

--- test/webdriver_logic/test/benchmarks/core.clj
+++ test/webdriver_logic/test/benchmarks/core.clj
@@ -1,4 +1,5 @@
-(ns webdriver-logic.test.benchmarks.core
+(ns ^{:doc "Benchmarks performed with criterium. Where quick-benchmark was used, this is noted."}
+  webdriver-logic.test.benchmarks.core
   (:refer-clojure :exclude [==])
   (:use webdriver-logic.core
         clojure.core.logic
@@ -11,9 +12,7 @@
 (defn start-browser
   []
   (let [d (start {:browser :chrome
-                  :cache-spec {:strategy :basic
-                               :args [{}]
-                               :include [ {:xpath "//a"} ]}
+
                   } *default-url*)]
     (set-driver! d)
     (raw/set-driver! d))
@@ -29,9 +28,9 @@
 
 (comment
 "
-     x86_64 Mac OS X 10.7.4 8 cpu(s)
-     Java HotSpot(TM) 64-Bit Server VM 20.8-b03-424
-     Runtime arguments: -agentlib:jdwp=transport=dt_socket,server=y,suspend=n -Dclojure.compile.path=/Users/semperos/dev/clojure/webdriver-logic/classes -Dwebdriver-logic.version=0.0.1-SNAPSHOT -Dclojure.debug=false
+x86_64 Mac OS X 10.8.2 8 cpu(s)
+Java HotSpot(TM) 64-Bit Server VM 23.3-b01
+Runtime arguments: -XX:+TieredCompilation -agentlib:jdwp=transport=dt_socket,server=y,suspend=n -Dclojure.compile.path=/Users/semperos/dev/clojure/webdriver-logic/target/classes -Dwebdriver-logic.version=0.0.1-SNAPSHOT -Dclojure.debug=false
 "
 )
 
@@ -39,11 +38,16 @@
 
 (comment
 "
-    Evaluation count             : 240
-             Execution time mean : 285.431004 ms  95.0% CI: (285.245250 ms, 285.684354 ms)
-    Execution time std-deviation : 27.940544 ms  95.0% CI: (27.790336 ms, 28.050204 ms)
-         Execution time lower ci : 239.179000 ms  95.0% CI: (239.179000 ms, 239.179000 ms)
-         Execution time upper ci : 332.341750 ms  95.0% CI: (332.341750 ms, 332.405100 ms)
+NOTE: The quick-benchmark function was used.
+Evaluation count             : 6
+             Execution time mean : 783.479833 ms  95.0% CI: (781.625833 ms, 784.059000 ms)
+    Execution time std-deviation : 43.967869 ms  95.0% CI: (43.643979 ms, 44.710451 ms)
+         Execution time lower ci : 730.513000 ms  95.0% CI: (730.513000 ms, 730.513000 ms)
+         Execution time upper ci : 838.903500 ms  95.0% CI: (837.897000 ms, 838.903500 ms)
+
+Found 1 outliers in 6 samples (16.6667 %)
+	low-severe	 1 (16.6667 %)
+ Variance from outliers : 14.3697 % Variance is moderately inflated by outliers
 ")
 (defn bench-tago
   []
@@ -52,11 +56,13 @@
 
 (comment
 "
-    Evaluation count             : 12420
-             Execution time mean : 4.838575 ms  95.0% CI: (4.838211 ms, 4.838844 ms)
-    Execution time std-deviation : 45.045486 us  95.0% CI: (44.396467 us, 45.573489 us)
-         Execution time lower ci : 4.789816 ms  95.0% CI: (4.789816 ms, 4.789816 ms)
-         Execution time upper ci : 4.894847 ms  95.0% CI: (4.894440 ms, 4.896366 ms)
+NOTE: The quick-benchmark function was used.
+Evaluation count             : 156
+             Execution time mean : 4.036173 ms  95.0% CI: (4.034782 ms, 4.038301 ms)
+    Execution time std-deviation : 108.084995 us  95.0% CI: (105.703304 us, 110.306433 us)
+         Execution time lower ci : 3.951577 ms  95.0% CI: (3.951577 ms, 3.951577 ms)
+         Execution time upper ci : 4.150712 ms  95.0% CI: (4.150712 ms, 4.176962 ms)
+Improvement: ~195 times
 ")
 (defn bench-tago-RAW
   []
@@ -67,11 +73,12 @@
 
 (comment
 "
-     Evaluation count             : 120
-              Execution time mean : 769.255192 ms  95.0% CI: (769.030392 ms, 769.496342 ms)
-     Execution time std-deviation : 31.689772 ms  95.0% CI: (31.579521 ms, 31.797228 ms)
-          Execution time lower ci : 715.613500 ms  95.0% CI: (715.613500 ms, 715.613500 ms)
-          Execution time upper ci : 812.841900 ms  95.0% CI: (812.757000 ms, 812.841900 ms)
+NOTE: The quick-benchmark function was used.
+Evaluation count             : 6
+             Execution time mean : 1.178506 sec  95.0% CI: (1.177556 sec, 1.179908 sec)
+    Execution time std-deviation : 65.447901 ms  95.0% CI: (64.942283 ms, 65.675600 ms)
+         Execution time lower ci : 1.123121 sec  95.0% CI: (1.123121 sec, 1.123121 sec)
+         Execution time upper ci : 1.245426 sec  95.0% CI: (1.245426 sec, 1.260944 sec)
 ")
 (defn bench-find-el-and-tag-via-attr
   []
@@ -83,11 +90,17 @@
 
 (comment
 "
-     Evaluation count             : 11640
-              Execution time mean : 5.132349 ms  95.0% CI: (5.132152 ms, 5.132576 ms)
-     Execution time std-deviation : 25.492576 us  95.0% CI: (25.256281 us, 25.723954 us)
-          Execution time lower ci : 5.097979 ms  95.0% CI: (5.097979 ms, 5.097979 ms)
-          Execution time upper ci : 5.187088 ms  95.0% CI: (5.187088 ms, 5.187849 ms)
+NOTE: The quick-benchmark function was used.
+Evaluation count             : 84
+             Execution time mean : 7.705119 ms  95.0% CI: (7.700595 ms, 7.709786 ms)
+    Execution time std-deviation : 1.046114 ms  95.0% CI: (1.043498 ms, 1.047432 ms)
+         Execution time lower ci : 7.186214 ms  95.0% CI: (7.186214 ms, 7.186214 ms)
+         Execution time upper ci : 9.169911 ms  95.0% CI: (9.169911 ms, 9.250625 ms)
+
+Found 1 outliers in 6 samples (16.6667 %)
+	low-severe	 1 (16.6667 %)
+ Variance from outliers : 31.7636 % Variance is moderately inflated by outliers
+Improvement: ~168 times
 ")
 (defn bench-find-el-and-tag-via-attr-RAW
   []
@@ -101,4 +114,10 @@
   [f]
   (start-browser)
   (report-result (benchmark (f)) :verbose)
+  (quit-browser))
+
+(defn run-quick-benchy
+  [f]
+  (start-browser)
+  (report-result (quick-benchmark (f)) :verbose)
   (quit-browser))
