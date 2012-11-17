f79e56244538eba56ffbb905adcd947ab025732e
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-11-16 22:48:57 -0500

    Mark version 0.0.2, upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject webdriver-logic "0.0.1-SNAPSHOT"
-  :description "Library for composing clj-webdriver tests using logic programming"
+(defproject webdriver-logic "0.0.2"
+  :description "Library of goals (relational functions) that can be used with core.logic to expose web pages as application domains for logic programs."
   :url "https://github.com/semperos/webdriver-logic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.logic "0.8.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/core.logic "0.8.0-beta2"]
                  [clj-webdriver "0.6.0-beta2"]
                  ;; [enlive "1.0.1"]
                  [org.clojars.semperos/enlive "1.0.1"]
